cafae82a9d68ed4c3d7f9dda57a02b4e74b3c00d
Henning P. Schmiedehausen <henning@schmiedehausen.org>
2024-12-10 10:53:23 -0800

    reformat

--- lualib/event.lua
+++ lualib/event.lua
@@ -2,7 +2,7 @@ local M = {}
 
 local event_names = {}
 for name, id in pairs(defines.events) do
-  event_names[id] = name
+    event_names[id] = name
 end
 
 --[[
@@ -11,47 +11,47 @@ end
 local handlers_for = {}
 
 local function dispatch(event)
-  for handler in pairs(handlers_for[event.name]) do
-    handler(event)
-  end
+    for handler in pairs(handlers_for[event.name]) do
+        handler(event)
+    end
 end
 
 function M.register(events, handler)
-  if type(events) ~= "table" then
-    events = {events}
-  end
-
-  for _, event_id in ipairs(events) do
-    -- debug("registering for " .. (event_names[event_id] or event_id))
-    local handlers = handlers_for[event_id]
-    if not handlers then
-      handlers = {}
-      handlers_for[event_id] = handlers
+    if type(events) ~= 'table' then
+        events = { events }
     end
 
-    if not next(handlers) then
-      script.on_event(event_id, dispatch)
-    end
+    for _, event_id in ipairs(events) do
+        -- debug("registering for " .. (event_names[event_id] or event_id))
+        local handlers = handlers_for[event_id]
+        if not handlers then
+            handlers = {}
+            handlers_for[event_id] = handlers
+        end
 
-    handlers[handler] = true
-  end
+        if not next(handlers) then
+            script.on_event(event_id, dispatch)
+        end
+
+        handlers[handler] = true
+    end
 end
 
 function M.unregister(events, handler)
-  if type(events) ~= "table" then
-    events = {events}
-  end
-
-  for _, event_id in ipairs(events) do
-    -- debug("unregistering for " .. (event_names[event_id] or event_id))
-    local handlers = handlers_for[event_id]
-    if handlers then
-      handlers[handler] = nil
-      if not next(handlers) then
-        script.on_event(event_id, nil)
-      end
+    if type(events) ~= 'table' then
+        events = { events }
     end
-  end
+
+    for _, event_id in ipairs(events) do
+        -- debug("unregistering for " .. (event_names[event_id] or event_id))
+        local handlers = handlers_for[event_id]
+        if handlers then
+            handlers[handler] = nil
+            if not next(handlers) then
+                script.on_event(event_id, nil)
+            end
+        end
+    end
 end
 
 return M
